db17b34aee4f50361f8196cbed5ff55b4107e78d
Ryan J. Parker <ryan.j.parker@wsu.edu>
2022-07-25 17:20:40 -0700

    finished simple hide and seek game with reset button

--- app.js
+++ app.js
@@ -11,35 +11,74 @@ const totalEl = document.getElementById('total');
 const lossesEl = document.getElementById('losses');
 const winsEl = document.getElementById('wins');
 
+const resetButton = document.getElementById('reset-button');
+
 // initialize state
 const hidingPlaces = ['tree', 'shed', 'boulder'];
 
 let correctGuesses = 0;
+let incorrectGuesses = 0;
 let totalGuesses = 0;
 
-shedButton.addEventListener('click', () => {
+function randomSpot() {
     const hidingSpot = Math.floor(Math.random() * 3);
     const answer = hidingPlaces[hidingSpot];
-    handleGuess(answer, 'shed');
+    return answer;
+}
+
+shedButton.addEventListener('click', () => {
+    handleGuess(randomSpot(), 'shed');
 });
 
 treeButton.addEventListener('click', () => {
-    const hidingSpot = Math.floor(Math.random() * 3);
-    const answer = hidingPlaces[hidingSpot];
-    handleGuess(answer, 'tree');
+    handleGuess(randomSpot(), 'tree');
 });
 
 boulderButton.addEventListener('click', () => {
-    const hidingSpot = Math.floor(Math.random() * 3);
-    const answer = hidingPlaces[hidingSpot];
-    handleGuess(answer, 'boulder');
+    handleGuess(randomSpot(), 'boulder');
 });
 
 function handleGuess(correctSpot, userGuess) {
+    clearFace();
+
+    if (correctSpot === userGuess) {
+        correctGuesses++;
+        totalGuesses++;
+        winsEl.textContent = correctGuesses;
+    } else {
+        incorrectGuesses++;
+        totalGuesses++;
+        lossesEl.textContent = incorrectGuesses;
+    }
+
+    if (correctSpot === 'tree') {
+        treeContainer.classList.add('face');
+    } else if (correctSpot === 'shed') {
+        shedContainer.classList.add('face');
+    } else {
+        boulderContainer.classList.add('face');
+    }
+
+    totalEl.textContent = totalGuesses;
+
+}
+
+resetButton.addEventListener('click', () => {
+    winsEl.textContent = 0;
+    lossesEl.textContent = 0;
+    totalEl.textContent = 0;
+    clearFace();
+});
+
+function clearFace() {
+    treeContainer.classList.remove('face');
+    shedContainer.classList.remove('face');
+    boulderContainer.classList.remove('face');
+}
+
     // reset the styles
     // then increment the guesses
     // then grab the appropriate container element for the correct guess from the DOM
     // then add the face class to that element so that the face shows up
     // then if the user guess is correct, increment the correct guesses
     // update the DOM to show this change to the user (including the losses, not tracked directly in state)
-}
